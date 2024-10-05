0fc505e802ce1e228c0909049f4fd4eb7bbe95fc
Ananya Dhiman <ananya.dhiman@somaiya.edu>
2024-10-05 17:40:10 +0530

    Almost Working just check the lost condition

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,18 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect,useState } from 'react';
 import './index.css'
 import { useImmer } from 'use-immer';
 import { getpoke } from './Api';
 
 
 
+
 export function Game() {
   console.log("Home Page...");
-  const [new_score,setNewscore]=useState(0);
-  const [max_score,setMax_core]=useState(0);
-  const [check,setCheck]=useState([0,0,0,0,0,0,0,0]);
+  var [new_score,setNew_score]=useState(0); 
+  var [max_score,setMax_score]=useState(0);
+  const [check,setCheck]=useImmer([0,0,0,0,0,0,0,0]);
+  const [final,setFinal]=useImmer([]);
   var x=0;
+  var count=-1;
+  var status=0;
   const [images,setImages]=useState([]);
-  const [data,setData]=useState([]);
+  const [data,setData]=useState([]);  
+  
   useEffect(() =>{
     async function fetchpoke(){
       try{
@@ -29,40 +34,131 @@ export function Game() {
     fetchpoke();
   },[]);
 
-  //Event handler function on clicking card
-  const handleEvent= () =>{
-  alert("Clicked!!");
- };
+ 
  
 
  //console.log(images);
  //console.log(data.results);
- var count=-1;
+
   
  //Making list of names and passing them through props
-  const final=images.map((image,index)=>{
-  const obj=data.results[index];
+ 
 
-  count++;
-  console.log(obj);
-  return{
-    "key":count,
-    "name": obj.name.toUpperCase(),
-    "url":image
+  if (images.length && data.results.length && final.length==0) {
+    setFinal(draft => {
+      draft.push(
+        ...images.map((image, index) => ({
+          key: index,
+          name: data.results[index].name.toUpperCase(),
+          url: image,
+        }))
+      );
+    });
   }
 
- 
-}
-);
+
+
+
+
 //Premature return statement:
 //The return statement in JavaScript should be on the same line as the object you're returning, or it will result in an implicit undefined return. In your current code, the return is on a line by itself, so nothing is returned from the function.
  console.log(final);
  
+ //To shuffle
+ function shuffle(draft) {
+    
+  var m = draft.length, t, i;
+
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = draft[m];
+    draft[m] = draft[i];
+    draft[i] = t;
+  }
+
+  return draft;
+}
+  
+
+   //Event handler function on clicking card
+   const handleEvent= (id) =>{
+    alert("Clicked!!"+id);
+    // update scoreboard using Immer
+    setCheck(draft=>{
+      draft[id]+=1;
+
+    });
+
+
+
+   
+
+    /*
+     setCheck(check[id]++); Wrong!!!!
+    How to do it using useState
+     const [check, setCheck] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
+
+    const handleEvent = (id) => {
+  
+    const newCheck = [...check];  
+    newCheck[id] += 1;
+    setCheck(newCheck);
+  */
+    max_score=Math.max(new_score,max_score);
+    setMax_score(max_score);
+    
+    setCheck(check);
+    for(var i=0;i<check.length;i++){
+      if(check[i]>1){
+        setCheck([0,0,0,0,0,0,0,0]);
+        status=1;
+
+        alert("You lost");
+        
+        break;
+
+      }
+
+    }
+    //Game goes on
+    if(status==0){
+     
+      setNew_score(new_score+1);
+
+
+    }
+    //Wrong card click
+    else if(status==1){
+      setNew_score(0);
+      status=0;
+
+    }
+
+
+    //Win condition
+    if(new_score==8){
+      alert("Congratulations,you won!");
+      setCheck([0,0,0,0,0,0,0,0]);
+      status=0;
+      setNew_score(0);
+    }
+  
+
+    
+    setFinal(draft => {
+      shuffle(draft); // Mutate the draft directly to shuffle
+    });
+
+   };
 
 
   const cards=final.map((poke)=>
     
-    <div key={poke.count} onClick={handleEvent} className="p">
+    <div key={poke.count} onClick={()=>handleEvent(poke.key)} className="p">
       <div className="icon">
       <img src={poke.url}></img>
 
@@ -148,3 +244,4 @@ export function Game() {
 
 //Scoring System- If all 8 cards are clicked only once Player wins (Reset scores) 
 //Else calc score and max score(that can be 8 max)
+
